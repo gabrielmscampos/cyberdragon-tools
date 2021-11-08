dbe4308e7963f75e98359731758cf2a6de875e16
Gabriel Moreira <gabrielmscampos@gmail.com>
2021-11-07 23:18:30 -0300

    updated smart contracts instances; roleToContract was a wrong code design now using workToContract; added Katrina in roleToName; fix fetchHeroInfo and fetchHeroWork to look both for New and Old smart contracts; updated fetchHeroIncome to discriminate when hero is not working

--- api/blockchain/chain.js
+++ api/blockchain/chain.js
@@ -6,48 +6,91 @@ import { binaryXAbi } from './abi.js';
 const web3 = new Web3("https://bsc-dataseed.binance.org");
 
 const Contracts = {
+  PlayInfo: new web3.eth.Contract(binaryXAbi["55cb"], gameCoreAddress.PlayInfo),
+  Mining: new web3.eth.Contract(binaryXAbi["2261"], gameCoreAddress.Mining),
   NewPlayInfo: new web3.eth.Contract(binaryXAbi["55cb"], gameCoreAddress.NewPlayInfo),
   NewMining: new web3.eth.Contract(binaryXAbi["2261"], gameCoreAddress.NewMining),
+  Datie: new web3.eth.Contract(binaryXAbi["3718"], workAddress.Datie),
+  Bulie: new web3.eth.Contract(binaryXAbi["661c"], workAddress.Bulie),
+  Tushu: new web3.eth.Contract(binaryXAbi["b835"], workAddress.Tushu),
+  Lgong: new web3.eth.Contract(binaryXAbi["f28f"], workAddress.Linggong),
   Blacksmith: new web3.eth.Contract(binaryXAbi["f28f"], workAddress.blacksmith),
   Rangework: new web3.eth.Contract(binaryXAbi["f28f"], workAddress.Rangework),
   Hunter: new web3.eth.Contract(binaryXAbi["de8d"], workAddress.hunter),
-  Bookmanger: new web3.eth.Contract(binaryXAbi["40ac"], workAddress.bookmanger)
+  Bookmanger: new web3.eth.Contract(binaryXAbi["40ac"], workAddress.bookmanger),
+  Gaoji: new web3.eth.Contract(binaryXAbi["c332"], workAddress.Gaoji),
+  Sixth: new web3.eth.Contract(binaryXAbi["c332"], workAddress.Sixth),
+  Seventh: new web3.eth.Contract(binaryXAbi["c332"], workAddress.Seventh)
 }
 
-const roleToContract = {
-  [roleAddress.Warrior]: Contracts.Blacksmith,
-  [roleAddress.Robber]: Contracts.Hunter,
-  [roleAddress.Mage]: Contracts.Bookmanger,
-  [roleAddress.Ranger]: Contracts.Rangework
+const workToContract = {
+  [workAddress.Datie]: Contracts.Datie,
+  [workAddress.Bulie]: Contracts.Bulie,
+  [workAddress.Tushu]: Contracts.Tushu,
+  [workAddress.Linggong]: Contracts.Lgong,
+  [workAddress.blacksmith]: Contracts.Blacksmith,
+  [workAddress.hunter]: Contracts.Hunter,
+  [workAddress.bookmanger]: Contracts.Bookmanger,
+  [workAddress.Rangework]: Contracts.Rangework,
+  [workAddress.Gaoji]: Contracts.Gaoji,
+  [workAddress.Sixth]: Contracts.Sixth,
+  [workAddress.Seventh]: Contracts.Seventh,
 }
 
 const roleToName = {
   [roleAddress.Warrior]: 'Warrior',
   [roleAddress.Robber]: 'Rogue',
   [roleAddress.Mage]: 'Mage',
-  [roleAddress.Ranger]: 'Ranger'
+  [roleAddress.Ranger]: 'Ranger',
+  [roleAddress.Katrina]: 'Katrina'
 }
 
 const fetchHeroInfo = async (tokenID) => {
-  return await Contracts.NewPlayInfo.methods.getPlayerInfoBySet(tokenID).call();
+  const heroInfo = await Contracts.NewPlayInfo.methods.getPlayerInfoBySet(tokenID).call();
+  if (heroInfo[1] === '0x0000000000000000000000000000000000000000') {
+    return await Contracts.PlayInfo.methods.getPlayerInfoBySet(tokenID).call();
+  } else {
+    return heroInfo
+  }
 };
 
 const fetchHeroWork = async (tokenID) => {
-  return await Contracts.NewMining.methods.getPlayerWork(tokenID).call();
+  const heroWork = await Contracts.NewMining.methods.getPlayerWork(tokenID).call();
+  if (heroWork.workType === '0x0000000000000000000000000000000000000000') {
+    return await Contracts.Mining.methods.getPlayerWork(tokenID).call();
+  } else {
+    return heroWork
+  }
 };
 
-const fetchHeroIncome = async (heroStatistics, heroRoleAddress, heroWorkStartBlock, currentBlockNumber) => {
-  const workContract = roleToContract[heroRoleAddress];
-  return await workContract.methods.getIncome(
-    heroStatistics,
-    heroWorkStartBlock,
-    currentBlockNumber
-  ).call(); // Args -> (heroStats, startTime, currentBlockNumber)
+const fetchHeroIncome = async (
+  heroStatistics,
+  heroWorkAddress,
+  heroWorkStartBlock,
+  currentBlockNumber
+  ) => {
+  if (heroWorkAddress === '0x0000000000000000000000000000000000000000') {
+    return {
+      value: '0000000000000000000000',
+      isWorking: false
+    }
+  } else {
+    const workContract = workToContract[heroWorkAddress];
+    const heroIncome = await workContract.methods.getIncome(
+      heroStatistics,
+      heroWorkStartBlock,
+      currentBlockNumber
+    ).call(); // Args -> (heroStats, startTime, currentBlockNumber)
+    return {
+      value: heroIncome,
+      isWorking: true
+    }
+  }
 };
 
 export {
   web3,
-  roleToContract,
+  workToContract,
   roleToName,
   fetchHeroInfo,
   fetchHeroWork,

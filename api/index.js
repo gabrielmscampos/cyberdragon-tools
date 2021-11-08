3d356d9e3661370209d03b8f3d9acd2b833f85b1
Gabriel Moreira <gabrielmscampos@gmail.com>
2021-11-07 23:18:45 -0300

    new tests under index.js of api

--- api/index.js
+++ api/index.js
@@ -22,7 +22,14 @@ let username = 'gamoreir';
 let account = '0x692d439f9451316276cF40717991F1c33BF175F2';
 let tokenID1 = '77048303454219599145288778338542229948499387929593178227373580014068752631246';
 let tokenID2 = '97088435566813472948411252872513847408227355424307362687991969674766917500162';
+let tokenID3 = '54920811139364593725549824606618877455993621990400323580078613040592177135519';
 let chatID = '123456'
+
+// Katrina level 1 not working (2021-09-07)
+let tokenID4 = '71136236125506732212258737774207465607559611659399690943572474877896966579657'
+
+// Katrina level 5 royal guard (2021-09-07)
+let tokenID5 = '86224047802425358601991411298201506029505464240674115846081659132714159734939'
 
 checkAccount(collectionName, username, account).then( async (res) => {
 
@@ -39,6 +46,9 @@ checkAccount(collectionName, username, account).then( async (res) => {
   // Add tokens
   await updateAccountTokens(collectionName, 'add', username, tokenID1);
   await updateAccountTokens(collectionName, 'add', username, tokenID2);
+  await updateAccountTokens(collectionName, 'add', username, tokenID3);
+  await updateAccountTokens(collectionName, 'add', username, tokenID4);
+  await updateAccountTokens(collectionName, 'add', username, tokenID5);
   res = await checkAccount(collectionName, username, account);
 
   console.log('Account', res);
@@ -49,18 +59,18 @@ checkAccount(collectionName, username, account).then( async (res) => {
 
   // console.log('Account', res);
 
-  // Update chatID
-  await updateAccountChat(collectionName, username, chatID);
-  res = await checkAccount(collectionName, username, account);
+  // // Update chatID
+  // await updateAccountChat(collectionName, username, chatID);
+  // res = await checkAccount(collectionName, username, account);
 
-  console.log('Account', res);
+  // console.log('Account', res);
 
   // Fetch tokens [CLOUDFLARE BLOCKING]
   // const tokens = await fetchWorkingTokens(account);
   // console.log(tokens);
 
   // Get hero info
-  const heroInfo = await fetchHeroInfo(tokenID1);
+  const heroInfo = await fetchHeroInfo(tokenID3);
   console.log('heroInfo', heroInfo);
 
   // Hero role name
@@ -68,19 +78,19 @@ checkAccount(collectionName, username, account).then( async (res) => {
   console.log('heroRoleName', heroRoleName);
 
   // Get hero work
-  const heroWork = await fetchHeroWork(tokenID1);
+  const heroWork = await fetchHeroWork(tokenID3);
   console.log('heroWork', heroWork);
 
   // Get hero income
   const currentBlockNumber = await web3.eth.getBlockNumber(); 
   const heroIncome = await fetchHeroIncome(
       heroInfo[0],
-      heroWork.careerAddr,
+      heroWork.workType,
       heroWork.startTime,
       currentBlockNumber
   );
   console.log('blockNumber', currentBlockNumber);
-  console.log('heroIncome', heroIncome, web3.utils.fromWei(heroIncome));
+  console.log('heroIncome', heroIncome.value, web3.utils.fromWei(heroIncome.value), heroIncome.isWorking);
 
   // Compute salary per block
   const primaryStats = getPrimaryStats(heroInfo[1], heroInfo[0]);
